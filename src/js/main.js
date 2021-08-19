a59231dcfca301af16452a603b55b20f18b40e0d
snaipp1 <snaipp1@yandex.by>
2021-08-19 15:50:04 +0300

    feat: add dynamic modal

--- src/js/main.js
+++ src/js/main.js
@@ -10,3 +10,36 @@ $('[data-count="second"]').on('click', ()=> {
 $('button').eq(2).on('click', () => {
     $('.w-500').fadeToggle(800);
 });
+
+$('#trigger').click(() => $('#trigger').createModal({
+    text: {
+        title: 'Modal title',
+        body: ' quod consequatur dignissimos nobis rem, assumenda explicabo saepe labore nisi, commodi eligendi architecto dolorem cupiditate ullam optio? In accusantium fugiat molestias.'
+    },
+    btns: {
+        count: 3,
+        settings: [
+            [
+                'Close',
+                ['btn-danger', 'mr-10'],
+                true
+            ],
+            [
+                'Save changes',
+                ['btn-success', 'mr-10'],
+                false,
+                () => {
+                    alert('Data has been save');
+                }
+            ],
+            [
+                'Test',
+                ['btn-primary', 'ml-10'],
+                false,
+                () => {
+                    alert('Hello World');
+                }
+            ]
+        ]
+    }
+}));
